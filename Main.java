c1e62013a69b091c775d8a7ba17ebcce325b805e
Connor Bailey <cbbcbail@gmail.com>
2015-01-18 09:06:04 -0700

    Initiated Main and Quadcopter

---
+++ Main.java
@@ -0,0 +1,11 @@
+package simulation;
+
+public class Main {
+	
+	public static void main(String[] args) {
+        
+		System.out.println("Welcome to the simulation");
+        
+	}
+    
+}
---
+++ Quadcopter.java
@@ -0,0 +1,21 @@
+package simulation;
+
+public class Quadcopter {
+
+    public static boolean isFlying;
+    public static int pos;
+    
+    public static int pos() {
+        
+        pos = 0;
+        
+    }
+    
+    public static boolean isFlying() {
+        
+        isFlying = true;
+        return isFlying;
+        
+    }
+
+}
