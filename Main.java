a3e33dfe24e36430d1c45e5606710686713f8dc6
Hayden Walker <haydenwalker123@gmail.com>
2015-01-20 22:50:25 -0700

    New Changes 1/20 HAYDEN
    Changed a bunch. Now all integer values are stored in order in the
    ArrayList<Integer> changeValues. This will allow us to then use these
    values easily as we turn them into RPMs. We also need to make it so
    that the changes of RPM do something, and that the simulated quadcopter
    doesn’t fly out of frame after like ten ticks.

--- Main.java
+++ Main.java
@@ -4,51 +4,61 @@ import javax.swing.*;
 import java.util.Scanner;
 import java.io.*;
 import java.util.*;
+import java.util.Stack;
 
 public class Main {
 	
+    
 	public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<String> lineArray = new ArrayList<String>();
-        lineArray = read();
+        ArrayList<String> lineArray = new ArrayList<String>(); //lineArray holds all values in text file, lines separated by commas
+        lineArray = read(); //uses read method to fetch data from text file
+        int bStringParsed = 0; //holds the value of bString after it is parsed
+        //int[] myIntArray = new int[lineArray.length];
+        int lineArraySize = lineArray.size();
+        int numValues = lineArraySize * 3;
+        List<Integer> changeValues = new ArrayList<Integer>(); // New ArrayList of Integers, used to store all integer values in text file, in order.
         
-        
+        //Opens JFrame
         JFrame f = new JFrame("Quadcopter Stimulation");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(1000, 1000);
 		System.out.println("Welcome to the stimulation");
         setup();
+        
         int i = 0;
         int j = 0;
-        while(i <= 20) {
-            String aString = lineArray.get(i);
-            System.out.println("ASTRING" + aString);
-            String[] aStringSplitted = aString.split(" ");
-            System.out.println("aStringSplitted Number One  " + aStringSplitted[0]);
-            while (j < 4){
-                String bString = aStringSplitted[j];
-                int final = Integer.parseInt(bString);
-                System.out.println(final);
+        int h = 0;
+        while(i <= 3) { //Takes in 4 lines from the text file. Can be raised if necessary.
+            String aString = lineArray.get(i); //String aString holds the string at point i in the ArrayList<String> lineArray
+            String[] aStringSplitted = aString.split(" ");//String Array aStringSplitted holds each of the three values on each line in the text file. These values, though numbers, are stored as string values.
+            while (j < 3){ //So this while loop parses the values into integers and stores the new values in a new
+                String bString = aStringSplitted[j]; //String bString stores the value of aStringSplitted at point j
+                bStringParsed = Integer.parseInt(bString);
+                System.out.println("bStringParsedTest" + bStringParsed);
+                changeValues.add(bStringParsed);
                 j++;
+                h++;
             }
+            j = 0;
             tick(f);
             i += 1;
+            h++;
         }
+        i = 0;
+        System.exit(0);
 	}
     
-    public static ArrayList<String> read() throws FileNotFoundException{
-        String sentence = "test";
+    public static ArrayList<String> read() throws FileNotFoundException{ //Returns value of type ArrayList<String>
+        String sentence = "test"; //sentence used to hold each line
         ArrayList<String> lineArray = new ArrayList<String>();
         Scanner fileScan;
         int x=0;
-        //String aString = "test";
         fileScan = new Scanner (new File("Change.txt"));
         while(fileScan.hasNext()){
             sentence = fileScan.nextLine();
-            System.out.println("SENTENCE:" + sentence);
-            lineArray.add(sentence);
+            lineArray.add(sentence); // add() appends each line to lineArray
         }
-        //System.out.println(lineArray[1]);
         return lineArray;
     }
     
