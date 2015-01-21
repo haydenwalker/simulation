71bf433750eb489c637673a09e6deb8f61e38a49
Hayden Walker <haydenwalker123@gmail.com>
2015-01-21 09:16:56 -0700

    New Code From Morning 1/21
    Now reads file, stores it all in array, and reads these values to
    determine next tick. Also uses images instead of a square (we may need
    to enlarge image). Still, the values are kind of hard to determine and
    it seems kind of random how it does it. Also the line is rewritten over
    to be from the origin (well, 100, 100). We need it to save the last
    point and make the new line from the point, not deleting the old line.

--- Main.java
+++ Main.java
@@ -5,31 +5,47 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.*;
 import java.util.Stack;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
 public class Main {
 	
     
 	public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<String> lineArray = new ArrayList<String>(); //lineArray holds all values in text file, lines separated by commas
-        lineArray = read(); //uses read method to fetch data from text file
-        int bStringParsed = 0; //holds the value of bString after it is parsed
-        //int[] myIntArray = new int[lineArray.length];
-        int lineArraySize = lineArray.size();
-        int numValues = lineArraySize * 3;
-        List<Integer> changeValues = new ArrayList<Integer>(); // New ArrayList of Integers, used to store all integer values in text file, in order.
-        
         //Opens JFrame
         JFrame f = new JFrame("Quadcopter Stimulation");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(1000, 1000);
-		System.out.println("Welcome to the stimulation");
+        System.out.println("Welcome to the stimulation");
         setup();
+        
+        List<Integer> changeValues = new ArrayList<Integer>();
+        changeValues = getValues();
+        
+        
+        int lineArraySize = changeValues.size() / 3;
+        for (int n = lineArraySize; n >= 0; n--){
+            tick(f);
+        }
+        
+            
+	}
+    
+    public static ArrayList<Integer> getValues() throws FileNotFoundException{
+        ArrayList<String> lineArray = new ArrayList<String>(); //lineArray holds all values in text file, lines separated by commas
+        lineArray = read(); //uses read method to fetch data from text file
+        int bStringParsed = 0; //holds the value of bString after it is parsed
+        int lineArraySize = lineArray.size();
+        int numValues = lineArraySize * 3;
+        ArrayList<Integer> changeValues = new ArrayList<Integer>(); // New ArrayList of Integers, used to store all integer values in text file, in order.
+        System.out.println("LINEARRAYSIZE" + lineArraySize);
         
         int i = 0;
         int j = 0;
         int h = 0;
-        while(i <= 3) { //Takes in 4 lines from the text file. Can be raised if necessary.
+        while(i <= (lineArraySize-1)) { //Takes in 4 lines from the text file. Can be raised if necessary.
             String aString = lineArray.get(i); //String aString holds the string at point i in the ArrayList<String> lineArray
             String[] aStringSplitted = aString.split(" ");//String Array aStringSplitted holds each of the three values on each line in the text file. These values, though numbers, are stored as string values.
             while (j < 3){ //So this while loop parses the values into integers and stores the new values in a new
@@ -41,28 +57,30 @@ public class Main {
                 h++;
             }
             j = 0;
-            tick(f);
             i += 1;
             h++;
         }
         i = 0;
-        System.exit(0);
-	}
+        return changeValues;
+    }
     
     public static ArrayList<String> read() throws FileNotFoundException{ //Returns value of type ArrayList<String>
         String sentence = "test"; //sentence used to hold each line
         ArrayList<String> lineArray = new ArrayList<String>();
         Scanner fileScan;
         int x=0;
-        fileScan = new Scanner (new File("Change.txt"));
+        fileScan = new Scanner (new File("../Desktop/simulation/Change.txt"));
         while(fileScan.hasNext()){
             sentence = fileScan.nextLine();
             lineArray.add(sentence); // add() appends each line to lineArray
         }
         return lineArray;
     }
-    
-    public static void tick(JFrame f) {
+    public static int k = 0;
+    public static void tick(JFrame f) throws FileNotFoundException {
+        
+        List<Integer> changeValues = new ArrayList<Integer>();
+        changeValues = getValues();
         
         Quadcopter.RPM1();
         Quadcopter.RPM2();
@@ -73,6 +91,14 @@ public class Main {
         Quadcopter.changeZ(Quadcopter.RPM1, Quadcopter.RPM2, Quadcopter.RPM3, Quadcopter.RPM4);
         Quadcopter.posZ();
         Quadcopter.isFlying(Quadcopter.posZ);
+        if (Quadcopter.posZ > 3){ //Makes sure it doesn't fly up out of screen (Gravity?)
+            Quadcopter.posZ -= 1;
+        }
+        
+        Quadcopter.changeX = changeValues.get(k);
+        Quadcopter.changeY = changeValues.get(k+1);
+        //Quadcopter.chanzeZ = changeValues.get(k+2);
+        
         Screen s = new Screen();
         f.add(s);
         f.setVisible(true);
@@ -82,7 +108,9 @@ public class Main {
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
-        
+        if (k < changeValues.size() - 3){
+            k += 3;
+        }
     }
     
     public static void setup() {
--- Screen.java
+++ Screen.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.util.*;
 import java.util.Scanner;
 import java.io.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
 public class Screen extends JPanel {
     
@@ -17,15 +19,20 @@ public class Screen extends JPanel {
     public int size = (10000/(500-Quadcopter.posZ()));
     
     public void paintComponent(Graphics g) {
-        //super.paint(g);
         super.paintComponent(g);
         this.setBackground(Color.WHITE);
         g.setColor(Color.BLACK);
-        g.fillRect(x-(size/2), y-(size/2), size, size);
+        //g.fillRect(x-(size/2), y-(size/2), size, size);
         boolean isItPrinting = false;
         if(Printer.isPrinting()) {
             isItPrinting = true;
         }
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("../Desktop/simulation/Quadcopter50x50.png"));
+        } catch (IOException e) {
+        }
+        g.drawImage(img, x-30, y-30, null);//Draws image at necessary point
         
         //g.drawLine(10, 10, 250 + ((1/2) * size), 250 + ((1/2) * size));
         g.drawLine(initX, initY, x + ((1/2) * size), y + ((1/2) * size));
